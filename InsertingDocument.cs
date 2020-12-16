2456c8341a3a2e2854a660a21e629fe7f4bf9bed
chandana89 <72071048+chandana89@users.noreply.github.com>
2020-12-16 18:26:11 +0530

    Create InsertingDocument.cs

---
+++ InsertingDocument.cs
@@ -0,0 +1,23 @@
+  
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.IO;
+using System.Linq;
+
+namespace ImpactedTestScript_CSharp
+{
+  class InsertingDocument
+  {
+      public InsertingDocument()
+      {
+      }
+      public void InsertMethods()
+      {
+      }
+      public String insertcommitData(){
+        return "";
+      }
+  }
+}
