63bcc31500715f65f6d242b90edcb39180cd98a5
chandana89 <72071048+chandana89@users.noreply.github.com>
2020-12-16 17:16:28 +0530

    Update AddToCartOrWishListBaseAction.java

--- AddToCartOrWishListBaseAction.java
+++ AddToCartOrWishListBaseAction.java
@@ -3,7 +3,6 @@ package com.konakart.actions;
 public class AddToCartOrWishListBaseAction extends BaseAction{
   protected int prodId=-1
   public int getProdId(){
-    //get
     return prodId;
   }
 
