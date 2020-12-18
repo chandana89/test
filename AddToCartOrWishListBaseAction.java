3a109490ae05a0f0889b95b6bc58d373fc42b3ac
chandana89 <72071048+chandana89@users.noreply.github.com>
2020-12-18 16:34:29 +0530

    added comment

--- AddToCartOrWishListBaseAction.java
+++ AddToCartOrWishListBaseAction.java
@@ -8,7 +8,7 @@ public class AddToCartOrWishListBaseAction extends BaseAction{
   }
 
   public void setProdId(int prodId){
-    //comment added12
+    //comment added123
     this.prodId=prodId;
   }
 }
