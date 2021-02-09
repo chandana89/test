ec0d7ce5da719c2cfc9191fd82c989eab1ffe016
chandana89 <72071048+chandana89@users.noreply.github.com>
2021-02-09 09:34:13 +0530

    Update AddToCartOrWishListBaseAction.java

--- AddToCartOrWishListBaseAction.java
+++ AddToCartOrWishListBaseAction.java
@@ -11,7 +11,7 @@ public class AddToCartOrWishListBaseAction extends BaseAction{
     this.prodId=prodId;
   }
   public void addToCart(){
-    //comment
+   
      int num=90;
     if(num){
       num=0;
