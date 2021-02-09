31bac36973a343739afef590a7a4c7d99d3d0620
chandana89 <72071048+chandana89@users.noreply.github.com>
2021-02-09 10:41:42 +0530

    Update LoginModel.cs

--- LoginModel.cs
+++ LoginModel.cs
@@ -7,7 +7,7 @@ namespace Grand.Web.Models.Customer
     public partial class LoginModel : BaseModel
     {
         //login
-        public string CheckoutAsGuet { get; set; }
+        public string CheckoutAsGuest { get; set; }
 
         [DataType(DataType.EmailAddress)]
         [GrandResourceDisplayName("Account.Login.Fields.Email")]
